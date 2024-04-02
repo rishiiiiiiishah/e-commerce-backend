37e0485b8b68b7c84e404ff0355dab7f8e612779
rishih shah <rishiiiishah@gmail.com>
2024-04-02 20:06:40 +0530

    hello world

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/upload",upload.single('product'),(req,res)=>{
 
     res.json({
         success:1,
-        image_url:`https://e-commerce-backend-t9xe.onrender.comimages/${req.file.filename}`
+        image_url:`https://e-commerce-backend-t9xe.onrender.com/images/${req.file.filename}`
     })
 })
 
